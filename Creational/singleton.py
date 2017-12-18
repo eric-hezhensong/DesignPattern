ad0a18b4c796c035e28987704015a225c30d7e52
hezhensong <hezhensong@bytedance.com>
2017-12-18 22:22:15 +0800

    单例修改

--- Creational/singleton.py
+++ Creational/singleton.py
@@ -1,17 +1,25 @@
 # coding:utf-8
 
 
-class Singoleton(object):
+class Singleton(object):
     """
     Design Pattern: Singleton
     """
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, '__instance'):
-            org = super(Singoleton, cls)
+            org = super(Singleton, cls)
             cls.__instance = org.__new__(cls, *args, **kwargs)
+
         return cls.__instance
+
+    def __init__(self):
+        pass
 
 
 if __name__ == '__main__':
-    pass
+    s1 = Singleton()
+    s2 = Singleton()
+
+    print s1.value
+    print s2.value
